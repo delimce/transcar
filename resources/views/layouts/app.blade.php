e8254f192b8acf48c79610dd4fbeff2e7b744c69
Luis De Lima <delimce@gmail.com>
2018-08-14 00:08:24 -0400

    adding navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,29 @@
     <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
 </head>
 <body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Transcar</a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><span class="glyphicon glyphicon-user"></span>{{session()->get("myUser")->nombre}}</li>
+            <li>
+                <a href="{!! url('logout') !!}"><span class="glyphicon glyphicon-log-in"></span> Editar datos</a>
+                &nbsp;|&nbsp;
+                <a href="{!! url('logout') !!}"><span class="glyphicon glyphicon-log-in"></span> Cerrar</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <!-- Content here -->
 <div id="wrapper">
-
     <!-- Sidebar -->
-    @component("components.menu",['person' => ''])
-    @endcomponent
-    <!-- /#sidebar-wrapper -->
+@component("components.menu",['person' => ''])
+@endcomponent
+<!-- /#sidebar-wrapper -->
 
     <!-- Page Content -->
     <div id="page-content-wrapper">
