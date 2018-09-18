a97484d5b45fda7a7c542528393e375b409bc132
Luis De Lima <delimce@gmail.com>
2018-09-17 20:14:32 -0400

    home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,8 +5,44 @@
     @component("components.pageTitle",['title' => 'Inicio'])
     @endcomponent
 
-    <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens,
-        and will appear non-collapsed on larger screens. When toggled using the button below, the menu will
-        appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+
+    <div class="row">
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">Control de asistencia</h3>
+                <h1><i class="fa fa-calendar-check-o" aria-hidden="true"></i></h1>
+                <p class="card-text">Carga de Asistencias o Inasistencias de empleados al día de hoy.</p>
+                <a href="{!! url('appear') !!}" class="btn btn-primary">Entrar al módulo</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">Registro de Producción</h3>
+                <h1><i class="fa fa-cubes" aria-hidden="true"></i></h1>
+                <p class="card-text">Carga de producción por mesa y linea del almacen.</p>
+                <a href="{!! url('prod') !!}" class="btn btn-primary">Entrar al módulo</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">Reportes</h3>
+                <h1><i class="fa fa-line-chart" aria-hidden="true"></i></h1>
+                <p class="card-text">Reportes para el pago de la nómina.</p>
+                <a href="#" class="btn btn-primary">Entrar al módulo</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">Administración</h3>
+                <h1><i class="fa fa-tachometer" aria-hidden="true"></i></h1>
+                <p class="card-text">Maestros y administración de los datos principales.</p>
+                <a href="{!! url('system') !!}" class="btn btn-primary">Entrar al módulo</a>
+            </div>
+        </div>
+
+    </div>
+
 @endsection
